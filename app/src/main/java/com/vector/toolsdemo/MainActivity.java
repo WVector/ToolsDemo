af0e229aa852e10124d3d41f97e85bd678dd124e
WVector <1964414877@qq.com>
2017-06-26 16:01:48 +0800

    分离Drawable工具

--- app/src/main/java/com/vector/toolsdemo/MainActivity.java
+++ app/src/main/java/com/vector/toolsdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.vector.toolsdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GradientDrawable gradientDrawable = new GradientDrawable();
+        // 设置绘画图片色值
+        gradientDrawable.setColor(Color.WHITE);
+        // 绘画的是矩形
+        gradientDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT);
+        // 设置矩形的圆角半径
+        gradientDrawable.setCornerRadius(30);
+
+        gradientDrawable.setStroke(4,Color.RED);
+
+        Button button = (Button) findViewById(R.id.btn);
+
+        button.setBackgroundDrawable(gradientDrawable);
+
     }
 }
